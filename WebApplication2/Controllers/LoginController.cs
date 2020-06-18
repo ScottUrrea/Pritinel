e823c843ad37ee8909811997fa084fa45a9dddd3
Scott Urrea <scott.urrea@gmail.com>
2020-06-18 12:37:22 -0400

    prueba

---
+++ WebApplication2/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+    }
+}
